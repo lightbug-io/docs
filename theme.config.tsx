033fa012b125ce56e3ff4878372e438ab92c94f0
Adam <adam@lightbug.io>
2023-07-26 09:25:24 +0100

    Lightbugify

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,35 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <div>
+      <img style={{ marginLeft: '.5em', width: '17px', height: '30px', display: 'inline-block'}} src="https://lightbug.io/images/logo-orange_hudcdce2ead9cbe2715b5cf652e648439f_53864_17x30_fit_q95_h2_box_3.webp" alt="Logo" />
+      <span style={{ marginLeft: '.5em', fontSize: "150%", verticalAlign: "bottom" }}>LightBug</span>
+    </div>
+  ),
+  logoLink: 'https://lightbug.io/',
+  head: (
+    <>
+      <link rel="icon" type="image/x-icon" href="https://lightbug.io/favicon.ico"></link>
+    </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/lightbug-io',
   },
   chat: {
-    link: 'https://discord.com',
+    link: '',
+    icon: '',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/lightbug-io/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'LightBug Documentation',
+  },
+  banner: {
+    text: 'Work in progress',
+    key: 'wip-001',
+  },
+  feedback: {
+    content: '',
   },
 }
 
