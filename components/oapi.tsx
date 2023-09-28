db3bfa5e15bd2543445852a70c0843831ed468bc
Adam <adam@lightbug.io>
2023-09-28 14:26:54 +0100

    add API key if provided

--- components/oapi.tsx
+++ components/oapi.tsx
@@ -1,6 +1,6 @@
 // Example from https://beta.reactjs.org/learn
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'openapi-explorer';
 import styles from './oapi.module.css'
 import OApiFiltering from './oapi-filtering.mdx'
@@ -21,12 +21,27 @@ declare global {
 export function OApi() {
   const specUrl = "/swagger/v1.json";
   const serverUrl = "https://api.lightbug.cloud/api";
+
+  useEffect(() => {
+    const explorer = document.querySelector('openapi-explorer');
+    const onSpecLoaded = (event: CustomEvent) => {
+      event.detail.info.title = "Lightbug API";
+      console.log("spec loaded: ", event.detail);
+    };
+    explorer?.addEventListener('spec-loaded', onSpecLoaded);
+
+    return () => {
+      explorer?.removeEventListener('spec-loaded', onSpecLoaded);
+    };
+  }, []);
+
   return <openapi-explorer
       className = {styles.oapi}
       spec-url = {specUrl}
       server-url = {serverUrl}
       collapse = {true}
       hide-server-selection = {true}
+      hide-schema-selection = {true}
       >
     <div slot="nav-section" className = {styles.customSectionBar}>Filtering</div>
     <div slot="custom-section" className = {styles.customSection}>
@@ -36,9 +51,9 @@ export function OApi() {
       <div className="nav-bar-section-title">Endpoints</div>
       <hr/>
     </div>
-    <div slot="authentication">
+    {/* <div slot="authentication">
       <div className = {styles.subheading}>AUTHENTICATION</div>
-      <div>Replaces the authentication section</div>
-    </div>
+      Token: <input type="text" value={authToken} onChange={handleAuthTokenChange} />
+    </div> */}
     </openapi-explorer>
 }
