839c71480b45cb436960dda56769f4fffbbe2fcd
Adam <adam@lightbug.io>
2023-10-20 15:17:59 +0100

    Finish the general layout and experince for settings

---
+++ components/helper-tools/sendreasons.jsx
@@ -0,0 +1,58 @@
+// Slightly tweaked version of https://chat.openai.com/share/2312e12b-9299-4274-a90a-eda395bc4650
+
+import React, { useState, useEffect } from "react";
+
+const ModeFlagsComponent = () => {
+  const [bitfieldValue, setBitfieldValue] = useState(0);
+  const bitfieldDescriptions = [
+    "Wake mode active",
+    "Sleep mode active",
+    "Bluetooth disconnected",
+    "Outside of Safe-zone",
+    "Motion detected",
+    "Device started moving",
+    "Device stopped moving",
+    "Position is stale: last known location was used"
+  ];
+
+  const calculateBitfield = (value) => {
+    const bits = [];
+    for (let i = 0; i < bitfieldDescriptions.length; i++) {
+      const bit = (value & (1 << i)) !== 0;
+      bits.push(bit);
+    }
+    return bits;
+  };
+
+  const [bitfieldBits, setBitfieldBits] = useState(calculateBitfield(bitfieldValue));
+
+  useEffect(() => {
+    setBitfieldBits(calculateBitfield(bitfieldValue));
+  }, [bitfieldValue]);
+
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      padding: "10px",
+    }}>
+      <input
+        type="number"
+        value={bitfieldValue}
+        onChange={(e) => setBitfieldValue(parseInt(e.target.value))}
+        style={{
+          padding: "5px",
+          borderRadius: "5px",
+          textAlign: "center"
+        }}
+      />
+      {bitfieldBits.map((bit, index) => (
+        <div key={index} style={{ margin: "1px" }}>
+          {bit ? "✅" : "❌"} {1 << index}: {bitfieldDescriptions[index]}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ModeFlagsComponent;
