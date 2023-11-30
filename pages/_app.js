24c2f778aa2d587372d71e5334a53dc1c5934295
Adam <adam@lightbug.io>
2023-11-30 13:45:04 +0000

    GA: Track page changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,33 @@
 import '../styles.css'
 import { ThemeProvider } from 'next-themes'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+import * as ga from '../lib/ga'
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return  (
     <ThemeProvider enableSystem={false} class="light" forcedTheme="nextra-docs">
       <Component {...pageProps} />
     </ThemeProvider>
   )
 }
+
+export default MyApp
