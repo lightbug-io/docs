2b3779ae592e243c048761a4d0a366fb2d59836b
Adam <adam@lightbug.io>
2024-06-24 14:09:50 +0100

    General review and update
    - Added FAQ for focus on tech and this docs site
    - Moved around overview pages, adding redirects
    - Cleaned up some wording
    - technology -> terminology

--- next.config.js
+++ next.config.js
@@ -7,5 +7,46 @@ module.exports = withNextra({
   output: 'export',
   images: {
     unoptimized: true
-  }
+  },
+  async redirects() {
+    return [
+      // 2024-06-24: Moved a series of "overview" pages under /platform
+      {
+        source: '/devices',
+        destination: '/platform/devices',
+        permanent: true,
+      },
+      {
+        source: '/devices/overview',
+        destination: '/platform/devices',
+        permanent: true,
+      },
+      {
+        source: '/apis',
+        destination: '/platform/apis',
+        permanent: true,
+      },
+      {
+        source: '/apis/overview',
+        destination: '/platform/apis',
+        permanent: true,
+      },
+      {
+        source: '/apps',
+        destination: '/platform/apps',
+        permanent: true,
+      },
+      {
+        source: '/apps/overview',
+        destination: '/platform/apps',
+        permanent: true,
+      },
+      // 2024-06-24: Moved "technology" to "terminology"
+      {
+        source: '/platform/technology/:path*',
+        destination: '/platform/terminology/:path*',
+        permanent: true,
+      },
+    ]
+  },
 })
