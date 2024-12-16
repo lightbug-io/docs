2d4ace713769696583255ef650524c7d0d19e730
Adam <adam@lightbug.io>
2024-12-16 15:35:28 +0000

    More options for protocol header code gen

--- components/ProtocolHeaders.vue
+++ components/ProtocolHeaders.vue
@@ -6,7 +6,6 @@
             <v-tab value="cpp">C++</v-tab>
             <v-tab value="ts">TypeScript</v-tab>
         </v-tabs>
-
         <v-card-text>
             <div v-if="activeTab === 'go'">
                 <v-tabs v-model="goFormat" bg-color="secondary">
@@ -14,27 +13,40 @@
                     <v-tab value="grouped">Grouped</v-tab>
                 </v-tabs>
                 <div v-if="goFormat === 'individual'">
-                    <highlightjs language="go" :code="goHeaderConstantsIndividual" />
+                    <highlightjs language="go" :code="computedGoHeaderConstantsIndividual" />
                 </div>
                 <div v-else>
-                    <highlightjs language="go" :code="goHeaderConstantsGrouped" />
+                    <highlightjs language="go" :code="computedGoHeaderConstantsGrouped" />
                 </div>
             </div>
             <div v-else-if="activeTab === 'cpp'">
-                <highlightjs language="cpp" :code="cppHeaderConstants" />
+                <highlightjs language="cpp" :code="computedCppHeaderConstants" />
             </div>
             <div v-else-if="activeTab === 'ts'">
-                <highlightjs language="typescript" :code="tsHeaderConstants" />
+                <highlightjs language="typescript" :code="computedTsHeaderConstants" />
             </div>
             <div v-else>
-                <highlightjs language="toit" :code="toitHeaderConstants" />
+                <highlightjs language="toit" :code="computedToitHeaderConstants" />
             </div>
+        </v-card-text>
+        <v-card-title>Options</v-card-title>
+        <v-card-text>
+            <v-text-field
+                v-model="prefix"
+                label="Prefix"
+                placeholder="Enter prefix"
+                @input="prefix = prefix.toUpperCase()"
+            />
+            <v-checkbox
+                v-model="showComments"
+                label="Show Comments"
+            />
         </v-card-text>
     </v-card>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
+import { defineComponent, ref, computed, onMounted } from 'vue';
 import jsyaml from 'js-yaml';
 import hljsVuePlugin from '@highlightjs/vue-plugin';
 
@@ -46,89 +58,94 @@ export default defineComponent({
     setup() {
         const activeTab = ref('go');
         const goFormat = ref('individual');
-        const goHeaderConstantsIndividual = ref('');
-        const goHeaderConstantsGrouped = ref('');
-        const cppHeaderConstants = ref('');
-        const tsHeaderConstants = ref('');
-        const toitHeaderConstants = ref('');
+        const prefix = ref('MH_');
+        const showComments = ref(true);
+        const headers = ref<any>({});
 
-        const generateGoConstants = (headers: any) => {
+        const computedGoHeaderConstantsIndividual = computed(() => {
             let goConstants = '';
-            for (const key in headers) {
-                const name = headers[key].name.toUpperCase().replace(/ /g, '_');
-                goConstants += `const MH_${name} = ${key}\n`;
+            for (const key in headers.value) {
+                const name = headers.value[key].name.toUpperCase().replace(/ /g, '_');
+                const description = headers.value[key].description || '';
+                goConstants += `const ${prefix.value}${name} = ${key}`;
+                if (showComments.value) {
+                    goConstants += ` // ${description}`;
+                }
+                goConstants += '\n';
             }
             return goConstants;
-        };
+        });
 
-        const generateGoConstantsGrouped = (headers: any) => {
+        const computedGoHeaderConstantsGrouped = computed(() => {
             let goConstants = 'const (\n';
-            for (const key in headers) {
-                const name = headers[key].name.toUpperCase().replace(/ /g, '_');
-                goConstants += `\tMH_${name} = ${key}\n`;
+            for (const key in headers.value) {
+                const name = headers.value[key].name.toUpperCase().replace(/ /g, '_');
+                const description = headers.value[key].description || '';
+                goConstants += `\t${prefix.value}${name} = ${key}`;
+                if (showComments.value) {
+                    goConstants += ` // ${description}`;
+                }
+                goConstants += '\n';
             }
             goConstants += ')';
             return goConstants;
-        };
+        });
 
-        const generateTsConstants = (headers: any) => {
+        const computedCppHeaderConstants = computed(() => {
+            let cppConstants = 'enum LB_MESSAGE_HEADERS : uint8_t {\n';
+            const headerKeys = Object.keys(headers.value);
+            headerKeys.forEach((key, index) => {
+                const name = headers.value[key].name.toUpperCase().replace(/ /g, '_');
+                const description = headers.value[key].description || '';
+                cppConstants += `\t${prefix.value}${name}\t= ${key}`;
+                if (showComments.value) {
+                    cppConstants += ` // ${description}`;
+                }
+                if (index < headerKeys.length - 1) {
+                    cppConstants += ',\n';
+                }
+            });
+            cppConstants += '\n}';
+            return cppConstants;
+        });
+
+        const computedTsHeaderConstants = computed(() => {
             let tsConstants = '';
-            for (const key in headers) {
-                const name = headers[key].name.toUpperCase().replace(/ /g, '_');
-                tsConstants += `export const MH_${name} = ${key};\n`;
+            for (const key in headers.value) {
+                const name = headers.value[key].name.toUpperCase().replace(/ /g, '_');
+                const description = headers.value[key].description || '';
+                tsConstants += `export const ${prefix.value}${name} = ${key};`;
+                if (showComments.value) {
+                    tsConstants += ` // ${description}`;
+                }
+                tsConstants += '\n';
             }
             return tsConstants;
-        };
+        });
 
-        const generateToitConstants = (headers: any) => {
+        const computedToitHeaderConstants = computed(() => {
             let toitConstants = '';
-            for (const key in headers) {
-                const name = headers[key].name.toUpperCase().replace(/ /g, '_');
-                toitConstants += `HEADER_${name} /int ::= ${key}\n`;
+            for (const key in headers.value) {
+                const name = headers.value[key].name.toUpperCase().replace(/ /g, '_');
+                const description = headers.value[key].description || '';
+                toitConstants += `${prefix.value}${name} /int ::= ${key}`;
+                if (showComments.value) {
+                    toitConstants += ` // ${description}`;
+                }
+                toitConstants += '\n';
             }
             return toitConstants;
-        };
+        });
 
         const loadHeaderConstants = async () => {
             try {
                 const response = await fetch('/files/protocol-v3.yaml');
                 const yamlData = await response.text();
                 const parsedData = jsyaml.load(yamlData);
-                const headers = parsedData.header;
-
-                // Generate Go constants
-                goHeaderConstantsIndividual.value = generateGoConstants(headers);
-                goHeaderConstantsGrouped.value = generateGoConstantsGrouped(headers);
-
-                // Generate C++ constants
-                let cppConstants = 'enum LB_MESSAGE_HEADERS : uint8_t {\n';
-                const headerKeys = Object.keys(headers);
-                headerKeys.forEach((key, index) => {
-                    const name = headers[key].name.toUpperCase().replace(/ /g, '_');
-                    cppConstants += `\tMH_${name}\t= ${key}`;
-                    if (index < headerKeys.length - 1) {
-                        cppConstants += ',\n';
-                    }
-                });
-                cppConstants += '\n}';
-                cppHeaderConstants.value = cppConstants;
-
-                // Generate TypeScript constants
-                tsHeaderConstants.value = generateTsConstants(headers);
-
-                // Generate Toit constants
-                toitHeaderConstants.value = generateToitConstants(headers);
+                headers.value = parsedData.header;
             } catch (error) {
                 console.error('Error loading YAML file:', error);
             }
-        };
-
-        const setActiveTab = (tab: string) => {
-            activeTab.value = tab;
-        };
-
-        const setGoFormat = (format: string) => {
-            goFormat.value = format;
         };
 
         onMounted(loadHeaderConstants);
@@ -136,13 +153,15 @@ export default defineComponent({
         return {
             activeTab,
             goFormat,
-            goHeaderConstantsIndividual,
-            goHeaderConstantsGrouped,
-            cppHeaderConstants,
-            tsHeaderConstants,
-            toitHeaderConstants,
-            setActiveTab,
-            setGoFormat,
+            prefix,
+            showComments,
+            computedGoHeaderConstantsIndividual,
+            computedGoHeaderConstantsGrouped,
+            computedCppHeaderConstants,
+            computedTsHeaderConstants,
+            computedToitHeaderConstants,
+            setActiveTab: (tab: string) => activeTab.value = tab,
+            setGoFormat: (format: string) => goFormat.value = format,
         };
     },
 });
